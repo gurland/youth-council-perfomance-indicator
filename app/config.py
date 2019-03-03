47441e5fc441f7486784baaa3d0c014a65219256
gurland <stas.gurland@gmail.com>
2019-03-04 01:33:32 +0200

    Add month numbers dictionary

--- app/config.py
+++ app/config.py
@@ -4,3 +4,8 @@ ANY = -1
 MEMBER = 0
 MODERATOR = 1
 ADMIN = 2
+
+MONTH_NUMBERS = {'january': 1, 'february': 2, 'march': 3,
+                 'april': 4, 'may': 5, 'june': 6, 'july': 7,
+                 'august': 8, 'september': 9, 'october': 10,
+                 'november': 11, 'december': 12}
